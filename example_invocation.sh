52cfc8e50e828b69153d49c9d15b14c71a49335e
Karl Voit <git@Karl-Voit.at>
2013-11-20 22:44:24 +0100

    moved image files to sub-folder

--- example_invocation.sh
+++ example_invocation.sh
@@ -1,6 +1,6 @@
 ./org-contacts2vcard.py --orgfile testdata/testcontacts.org \
     --targetfile testdata/testcontacts.vcf \
-    --imagefolder testdata \
+    --imagefolder testdata/images \
     --imageabbrev photo \
     $@   ## this adds everything you add as parameter to "example_invocation.sh" such as "-v"
 
